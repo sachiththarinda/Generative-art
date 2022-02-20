2cda92780554a718e7a69af5e6a8e335cee8f139
Sachith Tharinda <71630599+sachiththarinda@users.noreply.github.com>
2022-02-20 15:30:24 +0530

    draw lines

---
+++ generative_art.py
@@ -0,0 +1,41 @@
+import random
+from PIL import Image, ImageDraw
+
+def genertive_art():
+    print("generative art")
+    image_size_px =128
+    image_bg_color=(255, 255, 255)
+    image =Image.new("RGB", size=(image_size_px, image_size_px ), color=(image_bg_color))
+    
+    #draw some lines
+    draw = ImageDraw.Draw(image)
+    points = []
+    
+    #generate the points
+    for _ in range(10):
+        random_point=  (
+            random.randint(0, image_size_px), 
+            random.randint(0, image_size_px) 
+            )
+        points.append(random_point)
+       
+    
+    #draw the points
+    
+   
+    for i, point in enumerate(points):
+        p1 =point
+        
+        if i == len(points)-1:
+            p2=points[0]
+        else:
+            p2=points[i + 1]
+        line_xy= (p1, p2)
+        line_color=(0, 0, 0)
+        draw.line(line_xy, fill=line_color)
+    
+    image.save("test_image.png")
+    
+
+if __name__=="__main__":
+    genertive_art()
